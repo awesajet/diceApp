52356ff4fc27a5f3605003c4f51571502f95f5e7
ajet <awesajet@gmail.com>
2021-12-01 11:29:09 +0100

    First Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Dice'),
+          ),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+  void changeDice() {
+    setState(() {
+      rightDice = Random().nextInt(6) + 1;
+      leftDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image(
+                image: AssetImage('assets/images/dice$leftDice.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image(
+                image: AssetImage('assets/images/dice$rightDice.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
